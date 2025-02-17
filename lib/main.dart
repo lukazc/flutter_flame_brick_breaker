2f815cb5c83d2ce07f9277d41b305fce4debb19d
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-02-17 17:21:24 +0100

    Create Brick Breaker game and play area classes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'src/brick_breaker.dart';
+
 void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  runApp(GameWidgetMain());
+}
+
+class GameWidgetMain extends StatelessWidget {
+  const GameWidgetMain({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GameWidget(game: BrickBreaker());
+  }
 }
