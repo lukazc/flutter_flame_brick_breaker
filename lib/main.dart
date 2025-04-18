16c6cf9721578b85e786024e7455be510d5d3a22
Luka Žarković Černeli <luka.zarkovic.cerneli@gmail.com>
2025-04-18 10:31:32 +0200

    Implement GameApp widget with game state overlays

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
-import 'src/brick_breaker.dart';
+import 'src/widgets/game_app.dart';
 
 void main() {
-  runApp(GameWidgetMain());
-}
-
-class GameWidgetMain extends StatelessWidget {
-  const GameWidgetMain({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GameWidget(game: BrickBreaker());
-  }
+  runApp(GameApp());
 }
